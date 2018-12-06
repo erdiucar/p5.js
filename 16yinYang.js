1a3173facb84fd5cf6d54cedc49ba5e33f30c242
Erdi Uçar <erdi@erdiucar.com>
2018-12-06 07:15:05 +0300

    Add files via upload

---
+++ 16yinYang.js
@@ -0,0 +1,54 @@
+var diam;
+var theta;
+var renk;
+
+function setup()
+{
+    createCanvas(windowWidth, windowHeight);
+    diam = height / 5;
+}
+
+function draw()
+{
+    background(renkKoyudanMiBasliyor(true));
+    yinYang();
+}
+
+function yinYang()
+{
+    translate(mouseX, mouseY);
+    rotate(mouseX / 200);
+    fill(renkKoyudanMiBasliyor(true));
+    strokeWeight(0.5);
+    stroke(renkKoyudanMiBasliyor(false), 150);
+    arc(0, 0, diam, diam, PI / 2, PI + PI / 2);
+    fill(renkKoyudanMiBasliyor(false));
+    arc(0, 0, diam, diam, PI + PI / 2, TWO_PI + PI / 2);
+
+    noStroke();
+    fill(renkKoyudanMiBasliyor(true));
+    ellipse(0, -diam / 4, diam / 2, diam / 2);
+    fill(renkKoyudanMiBasliyor(false));
+    ellipse(0, diam / 4, diam / 2, diam / 2);
+
+    fill(renkKoyudanMiBasliyor(false));
+    ellipse(0, -diam / 4, diam / 8, diam / 8);
+    fill(renkKoyudanMiBasliyor(true));
+    ellipse(0, diam / 4, diam / 8, diam / 8);
+
+    theta += TWO_PI / frames;
+}
+
+function renkKoyudanMiBasliyor(renKoyuMu)
+{
+    if (renKoyuMu)
+    {
+        renk = map(mouseX, 0, width, 0, 255);
+    }
+    else
+    {
+        renk = map(mouseX, 0, width, 255, 0);
+    }
+
+    return renk;
+}
